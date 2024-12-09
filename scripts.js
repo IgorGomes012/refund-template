339f39056bdf39e81e6f31ac8eaadf41285cd145
igor gomes <gomesoliveirasl02@hotmail.com>
2024-12-09 10:18:37 -0300

    atualiza valor para padrao BRL

--- scripts.js
+++ scripts.js
@@ -1,8 +1,24 @@
 // seleciona os elementos do formulario
 const amount = document.getElementById("amount")
 
+//captura o evento input para formatar o valor
 amount.oninput = () => {
+  //obtem o valor atual do input e remove os caracteres
   let value = amount.value.replace(/\D/g, "");
 
-  amount.value = value
+  //transforma o valor em centavo.
+  value = Number(value) / 100
+
+  //atualiza o valor do input
+  amount.value = formatarCurrencyBRL(value)
 }
+
+function formatarCurrencyBRL(value){
+  //Formata o valor no padrao BRL
+  value = value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  })
+  return value
+}
+
