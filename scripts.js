654b66eb017195e7f2feef8e46f3474521643f7d
igor gomes <gomesoliveirasl02@hotmail.com>
2024-12-09 14:59:27 -0300

    create li

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 // seleciona os elementos do formulario
-const form = document.querySelector("form")
-const amount = document.getElementById("amount")
-const expense = document.getElementById("expense")
-const category = document.getElementById("category")
+const form = document.querySelector("form");
+const amount = document.getElementById("amount");
+const expense = document.getElementById("expense");
+const category = document.getElementById("category");
 
 //captura o evento input para formatar o valor
 amount.oninput = () => {
@@ -10,34 +10,47 @@ amount.oninput = () => {
   let value = amount.value.replace(/\D/g, "");
 
   //transforma o valor em centavo.
-  value = Number(value) / 100
+  value = Number(value) / 100;
 
   //atualiza o valor do input
-  amount.value = formatarCurrencyBRL(value)
-}
+  amount.value = formatarCurrencyBRL(value);
+};
 
-function formatarCurrencyBRL(value){
+function formatarCurrencyBRL(value) {
   //Formata o valor no padrao BRL
   value = value.toLocaleString("pt-BR", {
     style: "currency",
     currency: "BRL",
-  })
-  return value
+  });
+  return value;
 }
 
 // Captura o eveto de submit do formulario para obter os valores
 form.onsubmit = (event) => {
   // previne o comportamento padrão de recarregar a pagina
-  event.preventDefault()
+  event.preventDefault();
 
   //cria um objeto com os detalhes da nova dispesa
-    const newExpenses = {
-      id: new Date().getTime(),
-      expense: expense.value,
-      category_id: category.value,
-      category_name: category.options[category.selectedIndex].text,
-      amount: amount.value,
-      created_at: new Date(),
-    }
-}
+  const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
+    created_at: new Date(),
+  };
+  //chama a função que vai adicionar um item na lista
+  expenseAdd(newExpense)
+};
 
+
+function expenseAdd(newExpense) {
+  try {
+   //cria o elemento de li para adicionar na lista ul
+   const expenseItem = document.createElement("li")
+   expenseItem.classList.add("expense-item")
+
+  } catch (error) {
+    alert("nao foi possivel atualizar a lista de despesas")
+  }
+}
